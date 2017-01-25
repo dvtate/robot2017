aa213b4cb104da94f7022cf4d2ad640094856387
Dustin Van Tate Testa <toast27@gmail.com>
2017-01-25 14:49:35 -0800

    auto, ultrasonic, more modes, etc.

--- Robot.hpp
+++ Robot.hpp
@@ -8,8 +8,6 @@
 
 #include "WPILib.h"
 
-
-
 class Robot: public frc::IterativeRobot {
 
 private:
@@ -18,42 +16,41 @@ private:
 	RobotDrive myRobot; // drive train
 	Joystick xBox; // xbox360 controllers???
 	//Compressor airPump; // compressor
-	//Solenoid hamperLift; // piston to lift the hamper
 	//DoubleSolenoid gearGrabber; // this is what holds the gear in place
+	Ultrasonic sonar;
 	BuiltInAccelerometer accel; // accelerometer in the RoboRIO
 
 	frc::LiveWindow* lw = LiveWindow::GetInstance();
 
 	// multiple autonomous codes
 	frc::SendableChooser<std::string> chooser;
-	const std::string autoNameDefault = "Default";
-	const std::string autoNameCustom = "My Auto";
+	const std::string autoDriveForward = "drive forward 2s";
+	const std::string autoDoNothing = "sit and wait";
 	std::string autoSelected;
 
 public:
 
 	Robot();
-	/*
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * GetString line to get the auto name from the text box below the Gyro.
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the
-	 * if-else structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
-	 */
+
 	void RobotInit();
 	void AutonomousInit();
 	void AutonomousPeriodic();
 
 	void TeleopInit();
-
 	void TeleopPeriodic();
 
+	void TestInit()
+		{ std::cout <<"testing something?" <<std::endl; }
 	void TestPeriodic()
 		{ lw->Run(); }
+
+	void DisabledInit()
+		{ std::cout <<"Robot disabled" <<std::endl;	}
+	void DisabledPeriodic()
+	{
+		// put distance from ultrasonic in inches
+		frc::SmartDashboard::PutNumber("Distance: ", sonar.GetRangeInches());
+	}
 
 
 
